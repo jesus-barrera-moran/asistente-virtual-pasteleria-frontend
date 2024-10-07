18ce592681aefff7c7f35a1ff036360fcaf0df92
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-07 13:59:18 -0600

    Implementar paginacion a empleados

--- src/components/EmployeesManager.tsx
+++ src/components/EmployeesManager.tsx
@@ -15,8 +15,10 @@ import {
   Switch,
   Spinner,
   useToast,
+  Input,
+  Text, // Asegúrate de que Chakra UI esté importando el componente Text
 } from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons'; // Importamos el ícono
+import { CheckIcon } from '@chakra-ui/icons';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -36,6 +38,11 @@ const EmployeesManager: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [saving, setSaving] = useState<{ [key: number]: boolean }>({});
   const [changes, setChanges] = useState<{ [key: number]: boolean }>({});
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const [usersPerPage] = useState(5); // Usuarios por página
+  const [searchQuery, setSearchQuery] = useState(''); // Búsqueda
+  const [sortField, setSortField] = useState<string | null>(null); // Campo para ordenar
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc'); // Orden de clasificación
   const toast = useToast(); // Hook para mostrar mensajes de toast
   const router = useRouter();
   const currentUserRole = String(localStorage.getItem('id_rol'));
@@ -98,6 +105,33 @@ const EmployeesManager: React.FC = () => {
 
     fetchUsers();
   }, [toast, router]);
+
+  // Lógica de paginación
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+
+  // Filtrar los usuarios según la búsqueda
+  const filteredUsers = users.filter((user) =>
+    user.username?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.email?.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const sortedUsers = filteredUsers.sort((a, b) => {
+    if (!sortField) return 0;
+    
+    const aValue = (a[sortField as keyof User] as string) || ''; // Valor de 'a' o vacío si es null/undefined
+    const bValue = (b[sortField as keyof User] as string) || ''; // Valor de 'b' o vacío si es null/undefined
+
+    if (sortOrder === 'asc') {
+      return aValue.localeCompare(bValue);
+    } else {
+      return bValue.localeCompare(aValue);
+    }
+  });
+
+  // Usuarios para mostrar en la página actual
+  const currentUsers = sortedUsers.slice(indexOfFirstUser, indexOfLastUser);
+  const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
 
   const handleToggleEnabled = (id: number) => {
     setUsers((prevUsers) =>
@@ -171,6 +205,23 @@ const EmployeesManager: React.FC = () => {
     return rolesHierarchy[currentUserRole] >= rolesHierarchy[originalRole];
   };
 
+  const handleSort = (field: keyof User) => {
+    if (sortField === field) {
+      setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+    } else {
+      setSortField(field);
+      setSortOrder('asc');
+    }
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+  };
+
   if (loading) {
     return (
       <Flex justify="center" align="center" height="100vh">
@@ -188,55 +239,81 @@ const EmployeesManager: React.FC = () => {
       <Stack spacing={8} mx="auto" w="100%" bg="none" maxW="1000px" pt={16} px={6}>
         <Box w="100%" maxW="1000px" mx="auto" p={6} bg="none" rounded="lg">
           <Stack align="center">
+            {/* Campo de búsqueda */}
+            <Input
+              placeholder="Buscar por nombre de usuario o correo"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              mb={4}
+              maxW="300px"
+            />
             {users.length > 0 ? (
-              <Table variant="simple">
-                <Thead>
-                  <Tr>
-                    <Th>Nombre de Usuario</Th>
-                    <Th>Correo Electrónico</Th>
-                    <Th>Rol</Th>
-                    <Th>Estado</Th>
-                    <Th>Acciones</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {users.map((user) => (
-                    <Tr key={user.id}>
-                      <Td>{user.username}</Td>
-                      <Td>{user.email}</Td>
-                      <Td>
-                        <Select
-                          value={user.role}
-                          onChange={(e) => handleRoleChange(user.id, e.target.value)}
-                          isDisabled={isRoleReadOnly(user.role, originalRoles[user.id])}
-                        >
-                          <option value="11">Administrador</option>
-                          <option value="12">Empleado</option>
-                        </Select>
-                      </Td>
-                      <Td>
-                        <Switch
-                          colorScheme="teal"
-                          isChecked={user.enabled}
-                          onChange={() => handleToggleEnabled(user.id)}
-                          isDisabled={isRoleReadOnly(user.role, originalRoles[user.id])}
-                        />
-                      </Td>
-                      <Td>
-                        <Button
-                          colorScheme="blue"
-                          size="sm"
-                          onClick={() => handleSaveChanges(user)}
-                          isLoading={saving[user.id]}
-                          isDisabled={!changes[user.id]}
-                        >
-                          <CheckIcon boxSize={4} /> {/* Ícono de guardado */}
-                        </Button>
-                      </Td>
+              <>
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th cursor="pointer" onClick={() => handleSort('username')}>
+                        Nombre de Usuario {sortField === 'username' && (sortOrder === 'asc' ? '↑' : '↓')}
+                      </Th>
+                      <Th cursor="pointer" onClick={() => handleSort('email')}>
+                        Correo Electrónico {sortField === 'email' && (sortOrder === 'asc' ? '↑' : '↓')}
+                      </Th>
+                      <Th>Rol</Th>
+                      <Th>Estado</Th>
+                      <Th>Acciones</Th>
                     </Tr>
-                  ))}
-                </Tbody>
-              </Table>
+                  </Thead>
+                  <Tbody>
+                    {currentUsers.map((user) => (
+                      <Tr key={user.id}>
+                        <Td>{user.username}</Td>
+                        <Td>{user.email}</Td>
+                        <Td>
+                          <Select
+                            value={user.role}
+                            onChange={(e) => handleRoleChange(user.id, e.target.value)}
+                            isDisabled={isRoleReadOnly(user.role, originalRoles[user.id])}
+                          >
+                            <option value="11">Administrador</option>
+                            <option value="12">Empleado</option>
+                          </Select>
+                        </Td>
+                        <Td>
+                          <Switch
+                            colorScheme="teal"
+                            isChecked={user.enabled}
+                            onChange={() => handleToggleEnabled(user.id)}
+                            isDisabled={isRoleReadOnly(user.role, originalRoles[user.id])}
+                          />
+                        </Td>
+                        <Td>
+                          <Button
+                            colorScheme="blue"
+                            size="sm"
+                            onClick={() => handleSaveChanges(user)}
+                            isLoading={saving[user.id]}
+                            isDisabled={!changes[user.id]}
+                          >
+                            <CheckIcon boxSize={4} /> {/* Ícono de guardado */}
+                          </Button>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+                {/* Paginación */}
+                <Flex justify="space-between" align="center" mt={4} w="100%">
+                  <Button onClick={goToPreviousPage} disabled={currentPage === 1}>
+                    Anterior
+                  </Button>
+                  <Text>
+                    Página {currentPage} de {totalPages}
+                  </Text>
+                  <Button onClick={goToNextPage} disabled={currentPage === totalPages}>
+                    Siguiente
+                  </Button>
+                </Flex>
+              </>
             ) : (
               <p>No hay usuarios registrados</p>
             )}
